268ccc06132e90cf29d3a1de0bf160024cbc601d
masajinobe-ef <priscilla.effects@gmail.com>
2024-10-26 00:39:02 +0500

    up

--- src/config.py
+++ src/config.py
@@ -3,3 +3,8 @@ import os
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.path.join(PROJECT_DIR, '..', 'repos')
 MAX_CONCURRENT_DOWNLOADS = 4
+
+# Constants
+BRANCH_NAME = 'main'
+SUBMODULES_DIR = 'repos'
+GITMODULES_FILE = '.gitmodules'
--- src/git_operations.py
+++ src/git_operations.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
+import shutil
 from colorama import init, Fore, Style
+from loguru import logger
+from config import BRANCH_NAME, SUBMODULES_DIR, GITMODULES_FILE
 
 
 # Initialize colorama
@@ -9,15 +12,12 @@ init(autoreset=True)
 
 def show_help():
     print(f'{Fore.BLUE}💻 Usage: python main.py [options]{Style.RESET_ALL}\n')
-
     print(f'{Fore.YELLOW}Options:{Style.RESET_ALL}')
-
     print(f'{Fore.BLUE}--- Submodules ---{Style.RESET_ALL}')
     print('  -s, --sync             🔄 Sync submodules')
     print('  -c, --commit           📝 Commit changes in submodules')
     print('  -p, --push             🚀 Push changes in submodules')
     print('  -r, --remove ORG/REPO  ❌ Remove a specific submodule\n')
-
     print(f'{Fore.BLUE}--- Main Project ---{Style.RESET_ALL}')
     print(
         '  -m, --main-commit      💾 Commit changes in the main project only'
@@ -25,7 +25,6 @@ def show_help():
     print(
         '  -p-main, --push-main    🚀 Push changes in the main project only\n'
     )
-
     print(f'{Fore.BLUE}--- Danger Zone ---{Style.RESET_ALL}')
     print(
         '  -cm, --commit-all      🔥 Commit changes in both main project and submodules'
@@ -33,7 +32,6 @@ def show_help():
     print(
         '  -a, --all-push         🔥 Push all changes (submodules and main project)\n'
     )
-
     print(
         f'{Fore.YELLOW}⚠️ Warning: Commands that modify submodules can potentially overwrite changes.{Style.RESET_ALL}'
     )
@@ -41,37 +39,36 @@ def show_help():
 
 def check_error(result):
     if result.returncode != 0:
-        print(
-            f'{Fore.RED}Error: Failed to execute the previous command.{Style.RESET_ALL}'
-        )
+        logger.error('Failed to execute the previous command.')
         exit(1)
 
 
-def sync_submodules():
-    print(f'{Fore.GREEN}🔄 Updating submodules...{Style.RESET_ALL}')
-    result = subprocess.run(['git', 'submodule', 'update', '--remote'])
+def run_git_command(command):
+    result = subprocess.run(command, capture_output=True, text=True)
     check_error(result)
+    return result
+
+
+def sync_submodules():
+    logger.info('🔄 Updating submodules...')
+    run_git_command(['git', 'submodule', 'update', '--remote'])
 
 
 def commit_submodule_changes():
-    print(
-        f'{Fore.GREEN}🔍 Checking for changes in submodules...{Style.RESET_ALL}'
-    )
-    result = subprocess.run(
+    logger.info('🔍 Checking for changes in submodules...')
+    result = run_git_command(
         [
             'git',
             'submodule',
             'foreach',
             '--recursive',
             'git status --porcelain',
-        ],
-        capture_output=True,
-        text=True,
+        ]
     )
 
-    if result.returncode == 0 and result.stdout.strip():
+    if result.stdout.strip():
         commit_message = input('✏️ Enter commit message for submodules: ')
-        subprocess.run(
+        run_git_command(
             [
                 'git',
                 'submodule',
@@ -80,166 +77,107 @@ def commit_submodule_changes():
                 f"git add . && git commit -m '{commit_message}'",
             ]
         )
-        check_error(result)
+        logger.success('✅ Submodules committed successfully.')
     else:
-        print(
-            f'{Fore.GREEN}✅ No changes found in submodules.{Style.RESET_ALL}'
-        )
+        logger.info('✅ No changes found in submodules.')
 
 
 def push_submodule_changes():
-    print(f'{Fore.GREEN}🚀 Pushing changes in submodules...{Style.RESET_ALL}')
-    result = subprocess.run(
-        ['git', 'submodule', 'foreach', '--recursive', 'git push']
-    )
-    check_error(result)
+    logger.info('🚀 Pushing changes in submodules...')
+    run_git_command(['git', 'submodule', 'foreach', '--recursive', 'git push'])
 
 
 def commit_main_project():
-    print(
-        f'{Fore.GREEN}💾 Committing changes in the main project...{Style.RESET_ALL}'
-    )
+    logger.info('💾 Committing changes in the main project...')
     message = input('✏️ Enter commit message for the main project: ')
-    result = subprocess.run(['git', 'add', '.'])
-    check_error(result)
-    result = subprocess.run(['git', 'commit', '-m', message])
-    check_error(result)
+    run_git_command(['git', 'add', '.'])
+    run_git_command(['git', 'commit', '-m', message])
+    logger.success('✅ Main project committed successfully.')
 
 
 def push_main_project():
-    print(
-        f'{Fore.GREEN}🚀 Pushing changes in the main project...{Style.RESET_ALL}'
-    )
+    logger.info('🚀 Pushing changes in the main project...')
+    result = run_git_command(['git', 'remote', '-v'])
 
-    # Check for existing remotes
-    result = subprocess.run(
-        ['git', 'remote', '-v'], capture_output=True, text=True
-    )
-
-    # If there are no remotes, prompt for a URL
-    if result.returncode != 0 or not result.stdout.strip():
-        print(f'{Fore.RED}No remote repository configured.{Style.RESET_ALL}')
+    if not result.stdout.strip():
         remote_url = input(
             '🔗 Enter the remote repository URL (e.g., https://github.com/username/repo.git): '
         )
+        run_git_command(['git', 'remote', 'add', 'origin', remote_url])
+        logger.success(f'✅ Remote repository added as origin: {remote_url}')
 
-        # Add the remote repository as 'origin'
-        subprocess.run(['git', 'remote', 'add', 'origin', remote_url])
-        print(
-            f'{Fore.GREEN}✅ Remote repository added as origin: {remote_url}{Style.RESET_ALL}'
+    try:
+        run_git_command(
+            ['git', 'push', '--set-upstream', 'origin', BRANCH_NAME]
         )
+    except SystemExit:
+        logger.warning('Push failed. Attempting to pull changes...')
+        handle_push_failure()
 
-    # Attempt to push with --set-upstream
-    result = subprocess.run(
-        ['git', 'push', '--set-upstream', 'origin', 'main'],
-        capture_output=True,
-        text=True,
+
+def handle_push_failure():
+    pull_choice = input(
+        'Do you want to pull the latest changes from the remote? (y/n): '
     )
-
-    if result.returncode != 0:
-        if 'rejected' in result.stderr:
-            print(f'{Fore.RED}Error: Push rejected.{Style.RESET_ALL}')
-            print(result.stderr)
-            # Ask the user if they want to pull the latest changes
-            pull_choice = input(
-                'Do you want to pull the latest changes from the remote? (y/n): '
-            )
-            if pull_choice.lower() == 'y':
-                print(
-                    f'{Fore.YELLOW}Fetching and merging changes from the remote...{Style.RESET_ALL}'
-                )
-                fetch_result = subprocess.run(
-                    ['git', 'fetch', 'origin'], capture_output=True, text=True
-                )
-                merge_result = subprocess.run(
-                    ['git', 'merge', 'origin/main'],
-                    capture_output=True,
-                    text=True,
-                )
-
-                # Check for merge errors
-                if merge_result.returncode != 0:
-                    print(f'{Fore.RED}Error during merge: {Style.RESET_ALL}')
-                    print(merge_result.stderr)
-                    return
-
-                # Try pushing again
-                push_result = subprocess.run(['git', 'push', 'origin', 'main'])
-                check_error(push_result)
-
-            else:
-                print(
-                    f'{Fore.YELLOW}You chose not to pull changes. Please resolve the conflict manually.{Style.RESET_ALL}'
-                )
-        else:
-            check_error(result)
+    if pull_choice.lower() == 'y':
+        logger.info('Fetching and merging changes from the remote...')
+        run_git_command(['git', 'fetch', 'origin'])
+        run_git_command(['git', 'merge', f'origin/{BRANCH_NAME}'])
+        logger.info('Retrying push...')
+        run_git_command(['git', 'push', 'origin', BRANCH_NAME])
+    else:
+        logger.warning(
+            'You chose not to pull changes. Please resolve the conflict manually.'
+        )
 
 
 def remove_submodule(org_repo):
-    repo_path = f'repos/{org_repo}'
+    repo_path = os.path.join(SUBMODULES_DIR, org_repo)
     if not os.path.isdir(repo_path):
-        print(
-            f"{Fore.RED}Error: Repository '{repo_path}' does not exist as a submodule.{Style.RESET_ALL}"
+        logger.error(
+            f"Repository '{repo_path}' does not exist as a submodule."
         )
         exit(1)
 
-    print(
-        f"{Fore.YELLOW}🗑 Removing submodule '{org_repo}'...{Style.RESET_ALL}"
-    )
-    subprocess.run(
+    logger.info(f"🗑 Removing submodule '{org_repo}'...")
+    run_git_command(
         [
             'git',
             'config',
             '--file',
-            '.gitmodules',
+            GITMODULES_FILE,
             '--remove-section',
             f'submodule.{repo_path}',
         ]
     )
-    subprocess.run(
+    run_git_command(
         ['git', 'config', '--remove-section', f'submodule.{repo_path}']
     )
-    subprocess.run(['git', 'rm', '--cached', repo_path])
-    os.system(f'rm -rf {repo_path}')
-    subprocess.run(['git', 'add', '.gitmodules'])
-    subprocess.run(['git', 'commit', '-m', f'Remove submodule {org_repo}'])
-    print(
-        f"{Fore.GREEN}✅ Submodule '{org_repo}' removed successfully.{Style.RESET_ALL}"
-    )
+    shutil.rmtree(repo_path)
+    run_git_command(['git', 'rm', '--cached', repo_path])
+    run_git_command(['git', 'add', GITMODULES_FILE])
+    run_git_command(['git', 'commit', '-m', f'Remove submodule {org_repo}'])
+    logger.success(f"✅ Submodule '{org_repo}' removed successfully.")
 
 
 def commit_all_changes():
-    print(
-        f'{Fore.GREEN}💾 Committing changes in both the main project and all submodules...{Style.RESET_ALL}'
+    logger.info(
+        '💾 Committing changes in both the main project and all submodules...'
     )
     commit_main_project()
 
-    changes_found = False
-    repos_with_changes = []
-
-    # Check for changes in each submodule
-    result = subprocess.run(
+    result = run_git_command(
         [
             'git',
             'submodule',
             'foreach',
             '--recursive',
             'git status --porcelain',
-        ],
-        capture_output=True,
-        text=True,
+        ]
     )
-    if result.returncode == 0 and result.stdout.strip():
-        changes_found = True
-        repos_with_changes = [line for line in result.stdout.splitlines()]
-
-    if changes_found:
-        print(f'{Fore.YELLOW}Repositories with changes:{Style.RESET_ALL}')
-        for repo in repos_with_changes:
-            print(f'- {repo}')
-
+    if result.stdout.strip():
         commit_message = input('✏️ Enter commit message for submodules: ')
-        subprocess.run(
+        run_git_command(
             [
                 'git',
                 'submodule',
@@ -248,16 +186,17 @@ def commit_all_changes():
                 f"git add . && git commit -m '{commit_message}'",
             ]
         )
-        check_error(result)
+        logger.success('✅ All submodules committed successfully.')
     else:
-        print(
-            f'{Fore.GREEN}✅ No changes found in submodules.{Style.RESET_ALL}'
-        )
+        logger.info('✅ No changes found in submodules.')
 
 
 def push_all_changes():
-    print(
-        f'{Fore.GREEN}🔥 Pushing all changes (submodules and main project)...{Style.RESET_ALL}'
-    )
+    logger.info('🔥 Pushing all changes (submodules and main project)...')
     push_submodule_changes()
     push_main_project()
+
+
+if __name__ == '__main__':
+    show_help()
+
